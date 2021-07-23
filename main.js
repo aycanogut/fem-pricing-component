5f52eeef6937f6088c36423da5f22b4731cb8d03
Aycan Öğüt <aycanonfire@gmail.com>
2021-07-23 18:35:43 +0300

    updated classes

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ let price;
 document.querySelector('.container').innerHTML = `
 <div class="component">
     <header class="component__header">
-      <h1 class="component__title">
+      <h1 class="component__header__title">
         Simple, traffic-based pricing
       </h1>
-      <p class="component__text">\
+      <p class="component__header__text">\
         Sign-up for our 30-day trial. No credit card required.
       </p>
     </header>
@@ -27,17 +27,17 @@ document.querySelector('.container').innerHTML = `
           <input type="checkbox" class="component__toggle__input">
           <span class="component__toggle__switch"></span>
           <label class="component__toggle__yearly">Yearly Billing</label>
-          <span class="component__discount">${price}</span>
+          <span class="component__toggle__discount">${price}</span>
         </label>
       </section>
       <section class="component__footer">
-        <div class="component__text">
-          <p class="component__text__item">Unlimited Websites</p>
-          <p class="component__text__item">100% data ownership</p>
-          <p class="component__text__item">Email reports</p>
+        <div class="component__footer__text">
+          <p class="component__footer__text__item">Unlimited Websites</p>
+          <p class="component__footer__text__item">100% data ownership</p>
+          <p class="component__footer__text__item">Email reports</p>
         </div>
-        <div class="component__button">
-          <a href="#" class="component__button__item">Start my trial</a>
+        <div class="component__footer__button">
+          <a href="#" class="component__footer__button__item">Start my trial</a>
         </div>
       </section>
     </main>
