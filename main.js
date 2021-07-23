f55a2c128f25b35d0e6195a256e4125609d02aea
Aycan Öğüt <aycanonfire@gmail.com>
2021-07-23 19:28:14 +0300

    updated / component body class name

--- main.js
+++ main.js
@@ -15,31 +15,31 @@ document.querySelector('.container').innerHTML = `
       </p>
     </header>
     <main>
-      <section class="component__body">
-        <p>100k pageviews</p>
-        <label for="range" class="component__slider">
-          <input type="range" name="range" min="0" max="100" step="1" value="16" class="component__slider__input">
-          <output for="range" class="component__slider__output">$${16}.00</output>
-          <span class="component__slider__month">/ month</span>
-        </label>
-        <label class="component__toggle">
-          <label class="component__toggle__monthly">Monthly Billing</label>
-          <input type="checkbox" class="component__toggle__input">
-          <span class="component__toggle__switch"></span>
-          <label class="component__toggle__yearly">Yearly Billing</label>
-          <span class="component__toggle__discount">${price}</span>
-        </label>
-      </section>
-      <section class="component__footer">
-        <div class="component__footer__text">
-          <p class="component__footer__text__item">Unlimited Websites</p>
-          <p class="component__footer__text__item">100% data ownership</p>
-          <p class="component__footer__text__item">Email reports</p>
-        </div>
-        <div class="component__footer__button">
-          <a href="#" class="component__footer__button__item">Start my trial</a>
-        </div>
-      </section>
-    </main>
+    <section class="component__body">
+      <p>100k pageviews</p>
+      <label for="range" class="component__body__slider">
+        <input type="range" name="range" min="0" max="100" step="1" value="16" class="component__body__slider__input">
+        <output for="range" class="component__body__slider__output">$${16}.00</output>
+        <span class="component__body__slider__month">/ month</span>
+      </label>
+      <label class="component__body__toggle">
+        <label class="component__body__toggle__monthly">Monthly Billing</label>
+        <input type="checkbox" class="component__body__toggle__input">
+        <span class="component__body__toggle__switch"></span>
+        <label class="component__body__toggle__yearly">Yearly Billing</label>
+        <span class="component__body__toggle__discount">${price}</span>
+      </label>
+    </section>
+    <section class="component__body__footer">
+      <div class="component__body__footer__text">
+        <p class="component__body__footer__text__item">Unlimited Websites</p>
+        <p class="component__body__footer__text__item">100% data ownership</p>
+        <p class="component__body__footer__text__item">Email reports</p>
+      </div>
+      <div class="component__body__footer__button">
+        <a href="#" class="component__body__footer__button__item">Start my trial</a>
+      </div>
+    </section>
+    </main> 
   </div>
 `
