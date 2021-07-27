467b4e73adec22627ea84ffb57d43ceba20568f1
Aycan Öğüt <aycanonfire@gmail.com>
2021-07-27 19:25:14 +0300

    added / UI and slider wrapped into functions

--- main.js
+++ main.js
@@ -1,61 +1,64 @@
-// css import
 import './styles/style.css'
 
-let price;
-
-// main component DOM
-document.querySelector('.container').innerHTML = `
-<div class="component">
-  <header class="component__header">
-    <h1 class="component__header__title">
-      Simple, traffic-based pricing
-    </h1>
-    <p class="component__header__text">\
-      Sign-up for our 30-day trial. No credit card required.
-    </p>
-  </header>
-  <main class="component__body">
-    <section class="component__topper">
-      <h2 class="component__topper__pageviews">100k pageviews</h2>
-      <form>
-        <label for="slider" class="component__topper__slider">
-          <input type="range" name="slider" min="1" max="5" step="1" value="3" class="component__topper__slider__input">
-          <output for="slider" class="component__topper__slider__output">$${16}.00 <span class="component__topper__slider__month">/ month</span></output>
-        </label>
-        <label class="component__topper__toggle">
-          <p class="component__topper__toggle__monthly">Monthly Billing</p>
-            <label class="component__topper__toggle__switch">
-              <input type="checkbox" class="component__topper__toggle__switch__input">
-              <span class="component__topper__toggle__switch__slider"></span>
+// create main component on DOM
+const createUI = () => {
+  document.querySelector('.container').innerHTML = `
+    <div class="component">
+      <header class="component__header">
+        <h1 class="component__header__title">
+          Simple, traffic-based pricing
+        </h1>
+        <p class="component__header__text">\
+          Sign-up for our 30-day trial. No credit card required.
+        </p>
+      </header>
+      <main class="component__body">
+        <section class="component__topper">
+          <h2 class="component__topper__pageviews">100k pageviews</h2>
+          <form>
+            <label for="slider" class="component__topper__slider">
+              <input type="range" name="slider" min="1" max="5" step="1" value="3" class="component__topper__slider__input">
+              <output for="slider" class="component__topper__slider__output">$${16}.00 <span class="component__topper__slider__month">/ month</span></output>
             </label>
-          <p class="component__topper__toggle__yearly">Yearly Billing</p>
-          <span class="component__topper__toggle__discount">
-          </span> 
-        </label>
-      <form>
-    </section>
-    <section class="component__footer">
-      <ul class="component__footer__list">
-        <li class="component__footer__list__item">
-          <p class="component__footer__list__item__text">Unlimited Websites</p>
-        </li>  
-        <li class="component__footer__list__item">
-          <p class="component__footer__list__item__text">100% data ownership</p>
-        </li>  
-        <li class="component__footer__list__item">
-        <p class="component__footer__list__item__text">Email reports</p>
-        </li>  
-      </ul>
-      <div class="component__footer__button">
-        <a href="#" class="component__footer__button__item">Start my trial</a>
-      </div>
-    </section>
-  </main>
-</div>
+            <label class="component__topper__toggle">
+              <p class="component__topper__toggle__monthly">Monthly Billing</p>
+                <label class="component__topper__toggle__switch">
+                  <input type="checkbox" class="component__topper__toggle__switch__input">
+                  <span class="component__topper__toggle__switch__slider"></span>
+                </label>
+              <p class="component__topper__toggle__yearly">Yearly Billing</p>
+              <span class="component__topper__toggle__discount">
+              </span> 
+            </label>
+          <form>
+        </section>
+        <section class="component__footer">
+          <ul class="component__footer__list">
+            <li class="component__footer__list__item">
+              <p class="component__footer__list__item__text">Unlimited Websites</p>
+            </li>  
+            <li class="component__footer__list__item">
+              <p class="component__footer__list__item__text">100% data ownership</p>
+            </li>  
+            <li class="component__footer__list__item">
+            <p class="component__footer__list__item__text">Email reports</p>
+            </li>  
+          </ul>
+          <div class="component__footer__button">
+            <a href="#" class="component__footer__button__item">Start my trial</a>
+          </div>
+        </section>
+      </main>
+    </div>
 `
+};
+
+createUI();
 
 // change the slider background-color on change of the input slides
-const slider = document.querySelector('input[type = "range"]').oninput = function () {
-  let value = (this.value - this.min) / (this.max - this.min) * 100;
-  this.style.background = 'linear-gradient(to right, #A5F3EB 0%, #A5F3EB ' + value + '%, #EAEEFB ' + value + '%, #EAEEFB 100%)';
-}
+const slider = () => {
+  const slider = document.querySelector('input[type = "range"]').oninput = function () {
+    let value = (this.value - this.min) / (this.max - this.min) * 100;
+    this.style.background = 'linear-gradient(to right, #A5F3EB 0%, #A5F3EB ' + value + '%, #EAEEFB ' + value + '%, #EAEEFB 100%)';
+  }
+};
